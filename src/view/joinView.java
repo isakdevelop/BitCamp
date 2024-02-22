7825adff0dda92d46edca7d76de2a3c06fb8e78d
isakdevelop <daishi7462@naver.com>
2024-02-22 18:17:24 +0900

    feat - abstract Of Member(User)

---
+++ src/view/joinView.java
@@ -0,0 +1,33 @@
+package view;
+
+import builder.MemberBuilder;
+import model.MemberDTO;
+import service.UtilService;
+import serviceimpl.UtilServiceImpl;
+
+import java.util.Scanner;
+
+public class joinView {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("id, 비밀번호, 비밀번호 확인, 이름, 주민 번호," +
+                " 전화 번호, 주소, 직업을 나누어 입력해주세요.");
+
+        UtilService utilService = UtilServiceImpl.getInstance();
+
+        MemberDTO member = new MemberBuilder()
+                .userName(sc.next())
+                .password(sc.next())
+                .confirmPassword(sc.next())
+                .name(sc.next())
+                .socialSecurityNumber(sc.next())
+                .tell(sc.next())
+                .address(sc.next())
+                .job(sc.next())
+                .build()
+                ;
+
+        System.out.println(member.toString());
+    }
+}
